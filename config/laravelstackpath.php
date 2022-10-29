e8e45e0a164475d16e194e6c6fd20fa37bea423f
I'm Not Cry <44664655+Cryental@users.noreply.github.com>
2022-10-30 00:03:54 +0600

    Update laravelstackpath.php

--- config/laravelstackpath.php
+++ config/laravelstackpath.php
@@ -2,5 +2,5 @@
 
 return [
     'cache' => 'stackpath.proxies',
-    'url'   => 'https://gist.githubusercontent.com/Cryental/12d214eb1ded2047f7d584e4619089aa/raw/49377557635f626a23f8c7427bd2ae743415b7bb/stackpath.txt',
+    'url'   => 'https://k3t9x2h3.map2.ssl.hwcdn.net/ipblocks.txt',
 ];
